e48e449f136351d859ec3e3faa88c30ba6980a18
chelog <chelog@octo.gg>
2024-08-13 14:16:44 +0300

    refactor: remove non-existing command from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
-# Default the TZ environment variable to UTC.
 TZ=${TZ:-UTC}
 export TZ
 
-# Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
-# Switch to the container's working directory
 cd /home/container || exit 1
 
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
